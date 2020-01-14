c5aee1272ad0fdef5906303ece074c6f825d1942
lx <952847773@qq.com>
2020-01-14 15:23:14 +0800

    20200114-刘欣-功能2-测试分支提交代码

--- src/main/java/com/mage/crm/base/CrmConstant.java
+++ src/main/java/com/mage/crm/base/CrmConstant.java
@@ -2,6 +2,7 @@ package com.mage.crm.base;
 
 public class CrmConstant {
     //20200114-刘欣-功能1-修改的部分
+    //20200114-刘欣-功能2-测试分支提交代码
     public  static  final  Integer OPS_SUCCESS_CODE=200;
     public  static  final  String  OPS_SUCCESS_MSG="操作成功";
 
